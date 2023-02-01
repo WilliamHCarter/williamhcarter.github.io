56ea9d3326e8a893aee3d0c30202ca6c586c5367
WilliamHCarter <41378465+WilliamHCarter@users.noreply.github.com>
2023-02-01 11:00:52 -0500

    🎨: Update Favicons -Hotfix

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -10,7 +10,7 @@ const Main = ({children, router}) => {
             <title>Will Carter - Home</title>
             <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
             <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png/">
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png/"/>
         </Head>
         <Navbar path={router.asPath}/>
         <Container maxW="container.lg" pt={16}>
