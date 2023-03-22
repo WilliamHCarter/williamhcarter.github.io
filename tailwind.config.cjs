c03ae9b7a63e0c4ba58b3e9c91190ad56d932312
WilliamHCarter <41378465+WilliamHCarter@users.noreply.github.com>
2023-03-22 17:45:23 -0400

    ♻️: Reimplement Navbar in New Stack

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,15 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		colors: {
+			'light': '#5F92CC',
+			'dark': 'rgb(32 32 35)',
+			'darkhl': '#aaaaaa',
+			'offw': 'rgb(252, 250, 249)',
+		  },
+		  screens: {
+			'pc': '700px',
+		  },
 	},
 	plugins: [],
 }
