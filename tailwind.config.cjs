ab0273998b31720e2013f57ae62f0e575ad3660b
WilliamHCarter <41378465+WilliamHCarter@users.noreply.github.com>
2023-04-03 18:48:10 -0400

    ♻️: Barebones Mobile Menu
    -Used SolidJS to create a reactive menu for mobile

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,6 +9,8 @@ module.exports = {
 			'darkhl': '#525252',
 			'offw': 'rgb(252, 250, 249)',
 			'teal': '#2c7a7b',
+			'g': '#E2E8F0',
+			'ghl': '#CBD5E0',
 		  },
 		  screens: {
 			'pc': '700px',
