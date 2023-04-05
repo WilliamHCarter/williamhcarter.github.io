c658b1e97b1bbb3441b77dcb687030f853d823f9
WilliamHCarter <41378465+WilliamHCarter@users.noreply.github.com>
2023-04-05 19:19:10 -0400

    ⚡️: Speed and accessibility optimizations

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
 		extend: {},
 		colors: {
 			'light': '#5F92CC',
-			'dark': 'rgb(32 32 35)',
+			'dark': '#202023',
 			'darkhl': '#525252',
 			'offw': 'rgb(252, 250, 249)',
 			'teal': '#2c7a7b',
