465ef5d4e8fafcdf299e2b9ac42d576998998866
WilliamHCarter <41378465+WilliamHCarter@users.noreply.github.com>
2023-08-12 01:54:05 -0400

    🎨: Improve Contrast Ratios

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -15,6 +15,7 @@ module.exports = {
       darkhl: "#525252",
       offw: "rgb(252, 250, 249)",
       teal: "#2c7a7b",
+      dteal: "#3cacad",
       g: "#E2E8F0",
       ghl: "#CBD5E0",
       dnav: "rgba(32, 32, 35, 0.5)",
